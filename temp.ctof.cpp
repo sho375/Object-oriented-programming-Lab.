6c0c7d3bd4d153efb397790d451f3dff3a601232
Showkat Ahmad Beigh <148880500+sho375@users.noreply.github.com>
2023-12-04 20:31:56 +0530

    Create temp.ctof.cpp

---
+++ temp.ctof.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   float c,f;
+   cout<<"enter temperature in celsius";
+   cin>>c;
+   f=(c*9/5)+32;
+   cout<<"temperature in farenheit"<<f<<endl;
+   return 0;
+}
