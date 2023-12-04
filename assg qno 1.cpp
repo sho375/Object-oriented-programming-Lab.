a9486b528a05f4217948c34ed0e5c105b490975c
Showkat Ahmad Beigh <148880500+sho375@users.noreply.github.com>
2023-12-04 20:04:06 +0530

    Add files via upload

---
+++ assg qno 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float g,c;
+	cout<<"enter quantity in gallons";
+	cin>>g;
+	c=g/7.481;
+	cout<<"equivalent in cubic feet:"<<c;
+	return 0;
+}
---
+++ q no 3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=10;
+	cout<<x<<endl;
+	x=x*2;
+	cout<<x--<<endl;
+	
+	cout<<x<<endl;
+	return 0;
+}
